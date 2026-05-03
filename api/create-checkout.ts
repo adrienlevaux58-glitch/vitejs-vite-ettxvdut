177a0cd526d2350fada8e4e6c30e4b00b26c7c57
adrienlevaux58-glitch <adrienlevaux58@gmail.com>
2026-05-03 20:53:07 +0200

    fix stripe timeout

--- api/create-checkout.ts
+++ api/create-checkout.ts
@@ -3,6 +3,8 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2023-10-16',
+  timeout: 10000,
+  maxNetworkRetries: 0,
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
